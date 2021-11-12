efa3a830fef9430b8b091795f29308af1ac49b3b
jeffrepo <jeffer.leo93@gmail.com>
2021-11-12 11:40:13 -0600

    Cambios varios

--- models/res_company.py
+++ models/res_company.py
@@ -6,4 +6,7 @@ class res_company(models.Model):
     _inherit = 'res.company'
 
     encargado_contrato_id = fields.Many2one('hr.employee','Encargado contrato')
+    numero_registro_mercantil = fields.Char('Número de registro mercantil')
+    folio = fields.Char('Folio')
+    libro = fields.Char('Libro')
     nombre_notario = fields.Char('Nombre notario')
