7e2d881fff781f3c7ac23a5178424cb271f1c253
jeffrepo <jeffer.leo93@gmail.com>
2021-12-02 10:53:33 -0600

    Contratos

--- models/res_company.py
+++ models/res_company.py
@@ -10,3 +10,10 @@ class res_company(models.Model):
     folio = fields.Char('Folio')
     libro = fields.Char('Libro')
     nombre_notario = fields.Char('Nombre notario')
+    # SI es colegio otro formato
+    colegio = fields.Boolean('Colegio')
+    nombre_encargado = fields.Char('Nombre encargado')
+    edad_encargado = fields.Integer('Edad')
+    estado_civil_encargado = fields.Char('Estado civil')
+    dpi_encargado = fields.Char('DPI')
+    direccion_encargado = fields.Char('Direccion encargado')
